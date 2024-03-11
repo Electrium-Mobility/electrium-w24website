46f81120c43357dd3ea4259b8c48ad7f9d0d3d40
eysix <andrewau121@gmail.com>
2024-03-11 19:01:57 -0400

    finish application process section

--- src/components/JoinOurTeam/ApplicationProcess.tsx
+++ src/components/JoinOurTeam/ApplicationProcess.tsx
@@ -15,24 +15,44 @@ function ApplicationProcess() {
     return(
         <div className='relative md:py-24 py-16'>
             <div className="grid grid-cols-1 pb-8 text-center">
-                <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">How To Join</h3>
+                <h3 className="mb-4 md:leading-normal text-3xl leading-normal font-semibold">How To Join</h3>
 
                 <p className="text-slate-400 max-w-xl mx-auto">Ready to get building and take on new challenges? <br /> Apply today!</p>
             </div>
 
-            <div className="flex">
-            {steps.map((step, index) => (
-                <div
-                key={index}
-                className={`flex items-center justify-center rounded-full w-12 h-12 mr-4 border-2 cursor-pointer ${
-                    completedSteps.includes(index) ? 'bg-green-500 border-green-500' : 'border-gray-300'
-                }`}
-                onClick={() => toggleStep(index)}
-                >
-                <span className="text-lg font-bold">{index + 1}</span>
-                <span>{step}</span>
-                </div>
-            ))}
+            <div className="grid md:grid-cols-12 grid-cols-1 px-4 mt-8 gap-[30px]">
+                    <div className="lg:col-span-6 md:col-span-5">                            
+                    <ul className="list-none flex-row p-6 shadow dark:shadow-gray-800 rounded-md" id="myTab" role="tablist">
+                                <li>
+                                    <div className="px-6 py-4 ltr:text-left rtl:text-right rounded-md w-full transition-all duration-500 ease-in-out hover:bg-gray-100 hover:dark:bg-green-900 cursor-pointer">
+                                        <span className="block text-green-600 ">Step 1</span>
+                                        <span className="text-xl mt-2 block font-bold">Discover Electrium</span>
+                                        <span className="block mt-2">Learn more about our current projects and connect with us through social media!</span>
+                                    </div>
+                                </li>
+                                <li role="presentation">
+                                    <div className="px-6 py-4 ltr:text-left rtl:text-right rounded-md w-full transition-all duration-500 ease-in-out hover:bg-gray-100 hover:dark:bg-green-900 cursor-pointer">
+                                        <span className="block text-green-600">Step 2</span>
+                                        <span className="text-xl mt-2 block font-bold">Submit Your Application</span>
+                                        <span className="block mt-2">Tell us about yourself and why you're interested in joining Electrium! Applications open soon.</span>
+                                    </div>
+                                </li>
+                                <li role="presentation">
+                                    <div className="px-6 py-4 ltr:text-left rtl:text-right rounded-md w-full transition-all duration-500 ease-in-out hover:bg-gray-100 hover:dark:bg-green-900 cursor-pointer">
+                                        <span className="block text-green-600">Step 3</span>
+                                        <span className="block text-xl mt-2 font-bold">Interview With Us</span>
+                                        <span className="block mt-2">Chat with us so that we can get to know each other better! Meet our team and ask us questions!</span>
+                                    </div>
+                                </li>
+                            </ul>
+                    </div>
+                    <div className="lg:col-span-6 md:col-span-7">
+                        <div id="myTabContent" className="px-8 py-16 shadow dark:shadow-gray-800 rounded-md">
+                            <div className="" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <img src={require("/static/img/kickoff/kickoff.jpg").default} />
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
     );
