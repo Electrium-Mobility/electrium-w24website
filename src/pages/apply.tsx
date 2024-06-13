cb0b2d436df01c2490c09f1bb6b7a278fe0a380c
areuie <63759859+areuie@users.noreply.github.com>
2024-06-13 17:30:14 -0400

    Added role questions

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from "@docusaurus/Link";
 import clsx from "clsx";
 import Layout from '@theme/Layout';
@@ -7,6 +7,7 @@ import Heading from "@theme/Heading";
 import SquaredButton from '../components/UI Components/SquaredButton';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import roleSpecificJson from '../components/constants/role-specific-questions.json';
 
 const SELECT_PROGRAMS = [
     "Accounting and Financial Management",
@@ -133,9 +134,56 @@ const SELECT_ROLES = [
     "Finance",
 ];
 
+const SELECT_PROJECTS = [
+    "Electric Skateboard (4-month)",
+    "Electric Skateboard #2 (4-month)",
+    "OneWheel (4-month)",
+    "Carbon Fibre Skateboard (8-month)",
+    "Repair Team",
+    "Other"
+];
+
 const SELECT_TERMS = ["1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B"];
 
+interface RoleSpecificQuestion {
+    id: string;
+    role: string;
+    questions: {
+        name: string;
+        question: string;
+        type?: string;
+        options?: string[];
+    }[];
+}
+
+const roleSpecificQuestions: RoleSpecificQuestion[] = JSON.parse(
+    JSON.stringify(roleSpecificJson)
+);
+
 const ApplicationForm = () => {
+    const [roleQuestions, setRoleQuestions] = useState<RoleSpecificQuestion[]>([]);
+    const [selectedRole, setSelectedRole] = useState<string>(""); // State to track the selected role
+
+    useEffect(() => {
+        const roleData = roleSpecificQuestions.find(role => role.role === selectedRole);
+        if (roleData) {
+            const updatedQuestions = roleData.questions.map(question => ({
+                id: roleData.id, // Use roleData's id for each question
+                role: roleData.role,
+                questions: [
+                    {
+                        name: question.name,
+                        question: question.question,
+                        type: question.type || '', // Default to an empty string if type is undefined
+                        options: question.options || [], // Default to an empty array if options are undefined
+                    }
+                ],
+            }));
+            setRoleQuestions(updatedQuestions);
+        }
+    }, [selectedRole]);
+    
+
     const initialValues = {
         firstName: "",
         lastName: "",
@@ -148,6 +196,8 @@ const ApplicationForm = () => {
         inPerson: "",
         interests: "",
         heardSource: "",
+        roleQuestions: "",
+        friendReferal: "",
         comments: ""
     };
 
@@ -163,6 +213,8 @@ const ApplicationForm = () => {
         inPerson: true,
         interests: false,
         heardSource: true,
+        roleQuestions: true,
+        friendReferal: false,
         comments: false
     }
 
@@ -183,6 +235,8 @@ const ApplicationForm = () => {
         inPerson: Yup.string().required('Please select an option'),
         interests: Yup.string().optional(),
         heardSource: Yup.string().required('Please select an option'),
+        roleQuestions: Yup.string().required('Please select an option'),
+        friendReferal: Yup.string().optional(),
         comments: Yup.string().optional(),
     });
 
@@ -240,21 +294,53 @@ const ApplicationForm = () => {
                 {label} {required[name] && <span className="text-red-600">*</span>}
             </label>
             <label htmlFor={name} className="text-gray-500 text-sm">{caption}</label>
-            <Field type="checkbox" name={name} className="form-select mt-2 text-charcoal-600 border border-charcoal-300 rounded-md px-4 py-3 focus:outline-none focus:ring-green-700 focus:border-green-700">
-                <option value="">{"-Select option-"}</option>
+            <div className="p-4 bg-grey border-2 border-gray-300 rounded-md">
                 {options.map(option => (
-                    <option key={option} value={option}>{option}</option>
+                    <label key={option} className="block">
+                        <Field
+                            type="checkbox"
+                            name={name}
+                            value={option}
+                            className="form-checkbox text-charcoal-600 border border-charcoal-300 rounded-md focus:outline-none focus:ring-green-700 focus:border-green-700"
+                        />
+                        {option}
+                    </label>
                 ))}
-            </Field>
+            </div>
         </div>
     );
 
-    const applicationCaption = (
-        <>
-
-        </>
+    const RoleSpecificField = ({name, label, caption = <></>, options = []}) => (
+        <div className="grid grid-cols-1 mb-5">
+            <div className="grid grid-cols-1 mb-5">
+                <label htmlFor={name} className="font-semibold">
+                    {label} {required[name] && <span className="text-red-600">*</span>}
+                </label>
+                <label htmlFor={name} className="text-gray-500 text-sm">{caption}</label>
+                <select
+                    id={name}
+                    name={name}
+                    value={selectedRole}
+                    onChange={(e) => setSelectedRole(e.target.value)}
+                    className="form-select mt-2 text-charcoal-600 border border-charcoal-300 rounded-md px-4 py-3 focus:outline-none focus:ring-green-700 focus:border-green-700"
+                >
+                    {roleSpecificQuestions.map((roleQuestions) => (
+                        <option key={roleQuestions.id} value={roleQuestions.role}>
+                            {roleQuestions.role}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            {roleQuestions.map((questionData, index) => (
+                <div key={index} className="grid grid-cols-1 mb-5">
+                    {questionData.questions[0].type === '' && <TextField name={questionData.questions[0].name} label={questionData.questions[0].question}></TextField>}
+                    {questionData.questions[0].type === 'radio' && <RadioField name={questionData.questions[0].name} label={questionData.questions[0].question} options={questionData.questions[0].options}></RadioField>}
+                    {questionData.questions[0].type === 'dropdown' && <DropdownField name={questionData.questions[0].name} label={questionData.questions[0].question} options={questionData.questions[0].options}></DropdownField>}
+                    {questionData.questions[0].type === 'checkbox' && <CheckboxField name={questionData.questions[0].name} label={questionData.questions[0].question} options={questionData.questions[0].options}></CheckboxField>}
+                </div>
+            ))}
+        </div>
     );
-
 
     return (
         <Layout>
@@ -298,23 +384,17 @@ const ApplicationForm = () => {
                                                 <RadioField name="inPerson" label="Will you be in-person at Waterloo in Spring 2024?" options={["Yes", "No"]} />
                                                 <TextField name="interests"
                                                     label="What are your interests and hobbies? Tell us something interesting about yourself!"
-                                                    caption={
-                                                        <>
-                                                            This is for us to get to know you, and does not have an impact on your application :)
-                                                        </>
-                                                    }
-                                                    type="text"
+                                                    caption={<>This is for us to get to know you, and does not have an impact on your application :)</>}
                                                 />
                                                 <RadioField name="heardSource" label="How did you hear about Electrium Mobility?" options={SELECT_HEARD_SOURCE} />
-                                                <RadioField name="role" 
-                                                    label="What role are you interested in?"
-                                                    caption={
-                                                        <>
-                                                            You can learn more about what the various roles do <Link to="/contact" className="text-green-600 text-bold">here</Link>.
-                                                        </>
-                                                    }
-                                                    options={SELECT_ROLES}
-                                                />
+                                                <RoleSpecificField name="roleQuestions" label="What role are you interested in?" caption={<>You can learn more about what the various roles do <Link to="/contact" className="text-green-600 font-bold">here</Link>.</>} options={SELECT_ROLES}/>
+                                                <TextField name="friendReferral" label="If you're applying with a friend, please put their full name below." type="text"/>
+                                                <CheckboxField
+                                                    name="electriumProjects"
+                                                    label="Please selection which project(s) you're interested in. We will complete them during the Spring 2024 term."
+                                                    caption={<>Feel free to add your own project idea under "Other".</>}
+                                                    options={SELECT_PROJECTS}
+                                                ></CheckboxField>
                                                 <TextField name="comments" label="Any additional comments or questions?"/>
                                                 <button type="submit" className="btn p-2 w-48 inline-block align-middle bg-green-600 hover:bg-green-700 border-green-600 hover:border-green-700 text-white rounded-md w-full">Submit</button>
                                             </Form>
