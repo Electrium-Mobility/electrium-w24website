f307a00ef3c209829d06b567741b606c93bbc145
areuie <63759859+areuie@users.noreply.github.com>
2024-07-05 02:05:24 -0400

    Reversed application change

--- src/pages/join-our-team.tsx
+++ src/pages/join-our-team.tsx
@@ -15,7 +15,7 @@ function JoinOurTeam() {
                         <h3 className="mb-4 md:text-3xl md:leading-normal text-6xl leading-normal font-semibold">Join Our Team</h3>
                         <p className="text-slate-400 max-w-xl mx-auto">We are a community of students dedicated to learning and building cool things. Our focus is to create a welcoming, beginner-friendly environment for any students interested in personal electric vehicles. </p>
                         <div className="my-6">
-                            <LinkButton to="/apply">APPLY NOW</LinkButton>
+                            <LinkButton to="https://docs.google.com/forms/d/15Gi2L9ly5RnQlfEvdlhNgNd5azuqaPCltsDT_-ipoJw/">APPLY NOW</LinkButton>
                         </div>
                     </div>
                 </div>
